99ae984265830e3bd7f1a2a1d3ec9bf40ae9df6a
poi519 <iamyou519@mail.ru>
2015-01-27 02:14:01 +0300

    Game Over. First Beta

---
+++ src/game_over.lua
@@ -0,0 +1,24 @@
+local game_over = {}
+
+local BIG_FONT = love.graphics.newFont("resources/fonts/times.ttf", 64)
+local SMALL_FONT = love.graphics.newFont("resources/fonts/times.ttf", 16)
+function game_over.draw()
+  local h = love.graphics.getHeight()
+  local w = love.graphics.getWidth()
+
+  love.graphics.setFont(BIG_FONT)
+  love.graphics.printf(
+    "GAME OVER",
+    0, h / 2, w, 'center'
+  )
+end
+
+local t = 0
+local T_MAX = 3
+
+function game_over.update(dt)
+  t = t + dt
+  if t > T_MAX then set_current_stage(stages.game) end
+end
+
+return game_over
