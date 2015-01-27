7c0a727ff9fdf165256a8256edb4ae918485f5d8
poi519 <iamyou519@mail.ru>
2015-01-27 21:28:01 +0300

    Beta 2

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.width = 800
     t.window.height = 600
-    t.window.title = "50 градаций серого [BETA 1]"
+    t.window.title = "50 градаций серого [BETA 2]"
 end
 
